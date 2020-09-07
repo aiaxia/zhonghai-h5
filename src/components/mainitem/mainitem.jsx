67a435155807e20459a89b8c4fa599aeed0998ca
aiaxia <1075633455@qq.com>
2020-09-07 15:45:23 +0800

    原来遗留的 数据填充

--- src/components/mainitem/mainitem.jsx
+++ src/components/mainitem/mainitem.jsx
@@ -130,6 +130,78 @@ class Mainitem extends React.PureComponent {
               seradr: '北京市海淀区上地信息路11号1至4层整栋1幢三层西303室',
               oflshop: '',
               discont: ''
+            },
+            {
+              title: '北京雨辰梦文园林绿化有限公司',
+              sercon: '打造绿色办公空间，办公室绿化园艺服务',
+              sertime: '长久',
+              serphone: '010-59772033',
+              seradr: '北京市东城区和平里西街七区10号楼国土和平里招待所461',
+              oflshop: '',
+              discont: ''
+            },
+            {
+              title: '北京旭峰建设工程有限公司',
+              sercon: '空间设计、施工建造、软装服务全流程定制',
+              sertime: '长久',
+              serphone: '010-53382719',
+              seradr: '北京市丰台区玉泉营桥东北角陶瓷文化馆三层',
+              oflshop: '',
+              discont: ''
+            },
+            {
+              title: '中海物业',
+              sercon: '专属车位预留、酒店预订、形象岗迎宾、专车接送。',
+              sertime: '长久',
+              serphone: '010-59772033',
+              seradr: '北京中海广场',
+              oflshop: '',
+              discont: ''
+            },
+            {
+              title: '顺丰',
+              sercon: '上门代收代发公司、个人快递，安全可靠。',
+              sertime: '长久',
+              serphone: '17611775115',
+              seradr: '北京中海广场',
+              oflshop: '',
+              discont: ''
+            },
+            {
+              title: '国泰饭店',
+              sercon: '中海系商务酒店，将为COOC合作企业提供会务场地、团购房、个人优惠订房的特惠权益。',
+              sertime: '长久',
+              serphone: '010-65683366',
+              seradr: '北京市朝阳区建外街道永安西里12号',
+              oflshop: '国泰饭店',
+              discont: ''
+            },
+            {
+              title: '国泰饭店',
+              sercon: '利用中海商业旗下酒店等资源，亦可于项目内进行会务空间与服务的定制。',
+              sertime: '长久',
+              serphone: '010-65683366',
+              seradr: '北京市朝阳区建外街道永安西里12号',
+              oflshop: '国泰饭店',
+              discont: ''
+            },
+            {
+              title: 'OFFICEZIP',
+              sercon: '开放空置房源，提供其他地区企业分散办公；提供楼内企业会议室服务',
+              sertime: '长久',
+              serphone: '4001118001',
+              seradr: '北京中海广场',
+              oflshop: '',
+              discont: ''
+            },
+            {
+              title: '诺梵（上海）系统科技股份有限公司北京分公司',
+              sercon: '一、售后快速响应；二、提供诺梵专属高效办公策略wps调研服务、空间解决方案。',
+              sertime: '长久',
+              serphone: '13810969446',
+              seradr: '东亿国际传媒产业园演播大厅A座5层',
+              oflshop: '诺梵家具定制',
+              discont: '一、售后快速响应；二、提供诺梵专属高效办公策略wps调研服务、空间解决方案。'
             }
           ],
         },
@@ -154,6 +226,24 @@ class Mainitem extends React.PureComponent {
               seradr: '王四营桥南盛华宏林粮油批发市场1层花花市界A20号',
               oflshop: '馨花语花卉',
               discont: '享受专属优惠'
+            },
+            {
+              title: '世纪佳缘',
+              sercon: '一、线上世纪佳缘钻石会员9折优惠；二、线下约会吧免费体验券',
+              sertime: '2020/6/10-2020/8/31',
+              serphone: '',
+              seradr: '',
+              oflshop: '世纪佳缘',
+              discont: '一、线上世纪佳缘钻石会员9折优惠；二、线下约会吧免费体验券'
+            },
+            {
+              title: '滴滴出行',
+              sercon: '滴滴出行',
+              sertime: '2020/6/2-2020/6/29',
+              serphone: '',
+              seradr: '',
+              oflshop: '滴滴出行',
+              discont: '5折优惠'
             }
           ]
         }
