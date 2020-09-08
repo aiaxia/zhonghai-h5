88d762e838edc2e2e01584808e65ce5b2409276d
aiaxia <shine.xia.li@gmail.com>
2020-09-08 18:52:55 +0800

    首页按钮,右侧文本,字体替换

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -4,23 +4,49 @@ import * as React from 'react';
 import styles from './index.less';
 import mainImg from '../../assets/maintext.png'
 import startBtn from '../../assets/startBtn.png'
+import startBtn2 from '../../assets/startBtn2.png'
+import indexrTxt from '../../assets/indexrtxt.png'
 import Logobox from '../../components/logobox/logobox'
 
 class Index extends React.PureComponent {
   constructor (props) {
     super(props);
     this.state={
+      checked: true
     };
   }
+  componentWillUnmount=()=>{
+    clearInterval(this.timeout)
+  }
+  showMenu=()=>{
+    let t = this
+    this.setState({
+      checked: false
+    })
+    this.timeout = setTimeout(function(){
+      t.props.history.push({
+        pathname: "/menu"
+      })
+    },400)
+  }
+
   render() {
+    const {checked} = this.state
     return (
       <div className={styles.homeBox}>
         <Logobox />
         <div className={styles.mainImg}>
           <img src={mainImg} alt=""/>
         </div>
-        <div className={styles.startBtn}>
-          <Link to="/menu"><img src={startBtn} alt=""/></Link>
+        <div className={styles.rtxt}>
+          <img src={indexrTxt} alt=""/>
+        </div>
+        <div className={styles.startBtn} onClick={e=>this.showMenu()}>
+          {checked?
+          <img src={startBtn} alt=""/>:
+          <img src={startBtn2} alt=""/>
+          }
+          {/* <Link to="/menu"><img src={startBtn} alt=""/></Link> */}
         </div>
       </div>
     )
