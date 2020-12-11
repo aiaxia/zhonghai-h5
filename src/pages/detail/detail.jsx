599026d05faae26872ec8fcd3117a24dc33d91c8
aiaxia <shine.xia.li@gmail.com>
2020-12-11 12:33:38 +0800

    all

--- src/pages/detail/detail.jsx
+++ src/pages/detail/detail.jsx
@@ -1,6 +1,13 @@
 import { connect } from 'dva';
 import * as React from 'react';
 import styles from './detail.less';
+import topmain from '../../assets/topmain.png';
+import returnw from '../../assets/returnw.png';
+import master from '../../assets/master.png';
+import icon from '../../assets/icon.png';
+import epicure from '../../assets/epicure.png';
+import ungrander from '../../assets/ungrander.png';
+import artist from '../../assets/artist.png';
 
 class Detail extends React.PureComponent {
   constructor (props) {
@@ -15,8 +22,19 @@ class Detail extends React.PureComponent {
   render() {
     return (
       <div className={styles.detailBox}>
-        <div>
-          {/* <img src={} alt=""/> */}
+        <div className={styles.topmainBox}>
+          <img className={styles.topmain} src={topmain} alt=""/>
+          <img className={styles.returnw} src={returnw} alt=""/>
+        </div>
+        <div className={styles.mainBox}>
+          <ul>
+            <li><img src={master} alt=""/></li>
+            <li><img src={icon} alt=""/></li>
+            <li><img src={epicure} alt=""/></li>
+            <li><img src={ungrander} alt=""/></li>
+            <li><img src={artist} alt=""/></li>
+          </ul>
+          <div>项目物业：<a href="tel:010-59772083">010-5977 2083</a></div>
         </div>
       </div>
     )
